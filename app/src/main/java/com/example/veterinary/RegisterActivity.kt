d1960685fa6b9089d3351ed3e162c780896e6b3f
jubynni <126527873+jubynni@users.noreply.github.com>
2023-11-05 20:19:30 -0600

    Create RegisterActivity.kt

---
+++ app/src/main/java/com/example/veterinary/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.veterinary
+
+import android.app.Activity
+import android.os.Bundle
+
+class RegisterActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
